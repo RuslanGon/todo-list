628858addc098636a835d02f99db0cc8a136360d
RuslanGon <ruslan441006@gmail.com>
2025-04-15 13:05:03 +0300

    wer

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Create from "./Create.jsx";
 import  '../App.css'
 import axios from "axios";
+import { BsCircleFill, BsFillTrashFill } from 'react-icons/bs';
 
 const Home = () => {
   const [todos, setTodos] = useState([]);
@@ -22,14 +23,23 @@ const Home = () => {
     <div className="home">
       <h2>Todo List</h2>
       <Create />
-      { todos.length === 0 ? 
-      <div>
-        <h2>No Record</h2>
-      </div> 
-      :
-      todos.map((todo) => (
-        <div className="task">{todo.task}</div>
-      ))}
+      {todos.length === 0 ? (
+        <div>
+          <h2>No Record</h2>
+        </div>
+      ) : (
+        todos.map((todo) => 
+        <div className="task">
+            <div className="checkbox">
+                <BsCircleFill className='icon' />
+            {todo.task}
+            </div>
+            <div>
+                <span><BsFillTrashFill className='icon'/></span>
+            </div>
+            
+        </div>)
+      )}
     </div>
   );
 };
