16da2dda4a2aca7b40a93b11c536c30c1624151a
RuslanGon <ruslan441006@gmail.com>
2025-04-15 11:08:49 +0300

    wer

---
+++ client/src/components/Create.jsx
@@ -0,0 +1,11 @@
+
+const Create = () => {
+  return (
+    <div className="create_form">
+        <input type="text" />
+        <button type="button">Add</button>
+    </div>
+  )
+}
+
+export default Create
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import Create from "./Create.jsx";
+import  '../App.css'
+
+
+const Home = () => {
+  const [todos, setTodos] = useState([]);
+
+  return (
+    <div className="home">
+      <h2>Todo List</h2>
+      <Create />
+      { todos.length === 0 ? 
+      <div>
+        <h2>No Record</h2>
+      </div> 
+      :
+      todos.map((todo) => (
+        <div>{todo}</div>
+      ))}
+    </div>
+  );
+};
+
+export default Home;
